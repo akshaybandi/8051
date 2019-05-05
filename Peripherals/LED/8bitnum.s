3bb3241f30964d1e0cfe88220e72ae81e3552b86
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 20:00:27 +0530

    adding comments

--- Peripherals/LED/8bitnum.s
+++ Peripherals/LED/8bitnum.s
@@ -1,6 +1,7 @@
-leds equ p2
+; show 8 bit number on 8 leds
+leds equ p2   ; connect 8 led's across p2 port.
 cseg at 0
-mov a,#0xb3
-mov leds,a
+mov a,#0xb3   ; mov a,#number
+mov leds,a    
 sjmp $
 end
