d81292d5993508613894f94246dc714e80c8c45f
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 19:36:40 +0530

    adding comments

--- Duty Cycle/dutycycle50.s
+++ Duty Cycle/dutycycle50.s
@@ -1,9 +1,9 @@
-extrn code (delay_100ms)
+extrn code (delay_100ms)    ; delay_100ms subroutine is external to this file.
 cseg at 0
 L2:
-setb p1.1
-lcall delay_100ms
-clr p1.1
-lcall delay_100ms
-sjmp L2
+setb p1.1             ; set p1.0 bit
+lcall delay_100ms     ; 100 msec delay.
+clr p1.1              ; clear p1.0 bit
+lcall delay_100ms     ; 100 msec delay
+sjmp L2               ; repeat again
 end
