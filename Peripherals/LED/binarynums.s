5a9ce15a1866cf231231611ba8bd8f0e6ce1238e
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 20:02:56 +0530

    adding comments

--- Peripherals/LED/binarynums.s
+++ Peripherals/LED/binarynums.s
@@ -1,9 +1,10 @@
-leds equ p2
+; display 0 to 255 on 8 leds
+leds equ p2       ; connect 8 leds across port 2
 extrn code (delay_100ms)
 cseg at 0
 mov a,#0x00
 L1:
-mov leds,a
+mov leds,a        ; displaying 0 to 255 on 8 leds
 lcall delay_100ms
 lcall delay_100ms
 inc a
