24117f12df0cf78bc9fce32923f45fa4ecb6c781
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 19:22:16 +0530

    adding comments

--- Delay/delay_10ms.s
+++ Delay/delay_10ms.s
@@ -2,15 +2,15 @@
 cseg at 0
 acall delay_10ms
 
-delay_1ms:
+delay_1ms:    ; creates 1 msec of delay
 mov r0,#255
 djnz r0,$
 mov r0,#244
 djnz r0,$
 ret
 
-delay_10ms:
-mov r1,#10
+delay_10ms:   ; creates 10 msec of delay
+mov r1,#10    ; 1 msec x 10 = 10 msec
 L1:
 acall delay_1ms
 djnz r1,L1
