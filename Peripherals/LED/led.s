ad74b63bab3b2e6483bdf96f1b6a7da26046e214
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 20:05:26 +0530

    adding comments

--- Peripherals/LED/led.s
+++ Peripherals/LED/led.s
@@ -1,10 +1,11 @@
-led equ p1.0
+; led blinking after 100 msec 
+led equ p1.0        ; 1 led at port p1.0
 extrn code (delay_100ms)
 cseg at 0
 again:
-clr led
+clr led             ; led is off
 lcall delay_100ms
-setb led
+setb led            ; led is on
 lcall delay_100ms
 sjmp again
 end
