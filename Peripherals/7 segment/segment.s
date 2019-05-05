1d694fba11c1c9863ebe59f7e8db6dd8a682c7e0
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 19:56:19 +0530

    adding some comments, removing wrong sentences

--- Peripherals/7 segment/segment.s
+++ Peripherals/7 segment/segment.s
@@ -1,16 +1,15 @@
 extrn code (delay_1sec)
 
 cseg at 1000h
-lut : db 0xbf,0x86,0xdb,0xcf,0xe6,0xed,0xfd,0x87,0xff,0xef
-;lut : db 0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90
+lut : db 0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90    ; hex representation of 0 to 9 to be displayed on 7 segment.
 cseg at 0
-mov dptr,#lut
+mov dptr,#lut   ; moving addresss of lut (1000h) into dptr
 again:
-clr a
-movc a,@a+dptr
-mov p1,a
-mov a,dpl
-inc dptr
+clr a           ; clear accumulator
+movc a,@a+dptr  ; 0+1000h,0+1001h,0+1002h,0+1003h,0+1004h,0+1005h,0+1006h,0+1007h,0+1008h,0+1009h.
+mov p1,a        ; p1 <- a
+mov a,dpl       ; a <- dpl
+inc dptr        ; dptr + 1
 lcall delay_1sec
 cjne a,#09,again
 sjmp $
