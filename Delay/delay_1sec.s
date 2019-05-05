4219f98fb1186d6a99f72602ae3c98da1f84779f
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 19:25:02 +0530

    adding comments

--- Delay/delay_1sec.s
+++ Delay/delay_1sec.s
@@ -3,23 +3,23 @@ cseg at 0
 
 acall delay_1sec
 
-delay_1ms:
+delay_1ms:    ; creates 1 msec of delay
 mov r0,#255
 djnz r0,$
 mov r0,#241
 djnz r0,$
 ret
 
-delay_100ms:
-mov r1,#100
+delay_100ms:    ; creates 100 msec of delay
+mov r1,#100     
 L1:
-acall delay_1ms
+acall delay_1ms   ; 1 msec x 100 = 100 msec delay
 djnz r1,L1
 ret
 
-delay_1sec:
+delay_1sec:   ; creates 1 sec of delay
 mov r2,#10
 L2:
-acall delay_100ms
+acall delay_100ms   ; 100 msec x 10 = 1 sec delay
 djnz r2,L2
 end
