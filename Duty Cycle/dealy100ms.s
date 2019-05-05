d6509df6155cd634915943d9c4713a2561263ca8
akshaybandi <39883782+akshaybandi@users.noreply.github.com>
2019-05-05 19:38:43 +0530

    adding comments

--- Duty Cycle/dealy100ms.s
+++ Duty Cycle/dealy100ms.s
@@ -1,18 +1,18 @@
 delay segment code
 rseg delay
 
-delay_1ms:
+delay_1ms:      ; creates 1 msec of delay.
 mov r0,#255
 djnz r0,$
 mov r0,#244
 djnz r0,$
 ret
 
-delay_100ms:
+delay_100ms:    ; creates 100 msec delay.
 mov r1,#100
 L1:
 lcall delay_1ms
 djnz r1,L1
 ret
-public delay_100ms
+public delay_100ms    ; can accesible to other file.
 end
